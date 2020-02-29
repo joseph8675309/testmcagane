d5ca82a41df1d935cde5bb3e2a9a6618b18311a8
pooper <pooper@power_pc>
2020-02-29 11:55:07 -0800

    poopadded

---
+++ src/main/java/net/mcreator/tesstrmwork/MCreatorPoopGem.java
@@ -0,0 +1,44 @@
+
+package net.mcreator.tesstrmwork;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+@Elementstesstrmwork.ModElement.Tag
+public class MCreatorPoopGem extends Elementstesstrmwork.ModElement {
+	@ObjectHolder("tesstrmwork:poopgem")
+	public static final Item block = null;
+	public MCreatorPoopGem(Elementstesstrmwork instance) {
+		super(instance, 13);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64));
+			setRegistryName("poopgem");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
